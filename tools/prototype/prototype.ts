1fc6245113a0b01615363007b4bdb00b1a076476
thoschu <thoschulte@googlemail.com>
2022-07-29 16:53:37 +0200

    update

--- tools/prototype/prototype.ts
+++ tools/prototype/prototype.ts
@@ -1,13 +1,19 @@
-import { interval } from 'rxjs';
-import { buffer, map } from 'rxjs/operators';
+import { Observable, Observer, Subject } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
-const myInterval = interval(100);
+const obs$ = new Observable((observer: Observer<number>) => {
+  setTimeout(() => {
+    observer.next(Math.random());
+  }, 2000);
+}).pipe(delay(3000));
 
-const myBufferedInterval = myInterval.pipe(
-  map((val: number) =>  val),
-  buffer(interval(3000))
-);
+obs$.subscribe((res: number) => {console.dir(res)});
+obs$.subscribe((res: number) => {console.dir(res)});
 
-const subscribe = myBufferedInterval.subscribe(val =>
-  console.log( val)
-);
+const sub = new Subject();
+
+sub.next(1);
+sub.subscribe(x => console.log('Subscriber A', x));
+sub.next(2); // OUTPUT => Subscriber A 2
+sub.subscribe(x => console.log('Subscriber B', x));
+sub.next(3);
