aba681912554f227eeebb26e6f83ef29f6d8f44d
thoschu <thoschulte@googlemail.com>
2022-08-13 18:34:47 +0200

    make class self calling

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  host: '0.0.0.0'
 };
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,20 +1,40 @@
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
-// import { environment } from './environments/environment';
+import { environment } from './environments/environment';
 
-(async () => {
-  const globalPrefix = 'api';
-  const defaultPort = 3333;
-  const port = process.env.PORT || defaultPort;
+class Main implements Object {
+  private static app: INestApplication;
+  private static readonly globalPrefix = 'api';
+  private static readonly defaultPort = 3333;
+  private static readonly host = environment.host;
+  private static readonly port = process.env.PORT || Main.defaultPort;
+  private static readonly appPromise: Promise<INestApplication> = NestFactory.create(AppModule);
+  protected static readonly running: Main = new Main();
 
-  const app = await NestFactory.create(AppModule);
+  constructor() {
+    Main.init();
 
-  app.setGlobalPrefix(globalPrefix);
+    console.log(environment);
+  }
 
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
-})();
+  private static async init(): Promise<void> {
+    Main.app = await Main.appPromise;
+    Main.setGlobalPrefix();
+    await Main.listen();
+  }
+
+  private static setGlobalPrefix(): void {
+    Main.app.setGlobalPrefix(Main.globalPrefix);
+  }
+
+  private static async listen() {
+    await Main.app.listen(Main.port, () => {
+      const logMessage = `Listening at http://${Main.host}:${Main.port}/${ Main.globalPrefix}`;
+
+      Logger.log(logMessage);
+    });
+  }
+}
